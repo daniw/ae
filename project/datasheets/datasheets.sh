4e018691875ef4d93cc24fd0231438b9084cbda2
daniw <dani.winz@bluewin.ch>
2014-11-15 01:09:00 +0100

    Datasheets

---
+++ project/datasheets/datasheets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### Multiplier
+
+# OPA835 -> Operational amplifier
+wget -nc http://www.ti.com/lit/ds/symlink/opa835.pdf
+wget -nc http://www.ti.com/lit/an/sloa162/sloa162.pdf
+# TS3021 -> Comparator
+wget -nc -O ts3021.pdf http://www.st.com/web/en/resource/technical/document/datasheet/CD00118313.pdf
+# TS5A3157 -> Analog switch SPDT
+wget -nc http://www.ti.com.cn/cn/lit/ds/symlink/ts5a3157.pdf
+
