feec209be1783ad9c2c51b369f19537bf41ed9ce
daniw <dani.winz@bluewin.ch>
2015-10-26 00:19:56 +0100

    Plot Amplitudenspektrum

--- hs15/fillcap/mess/mess01.m
+++ hs15/fillcap/mess/mess01.m
@@ -10,23 +10,35 @@ data3 = csvread('mess01/scope_3.csv');
 figure(1);
 plot(data0(:,1), data0(:,2));
 title('1kHZ .. 100MHz mit Wasser');
-grid on;
+xlabel('Frequenz');
+ylabel('Amplitude');
+axis([-5 5 -6 6]);
+grid minor;
 print -dpdf 'scope_0'
 
 figure(2);
 plot(data1(:,1), data1(:,2));
 title('1kHZ .. 100MHz ohne Wasser');
-grid on;
+xlabel('Frequenz');
+ylabel('Amplitude');
+axis([-5 5 -6 6]);
+grid minor;
 print -dpdf 'scope_1'
 
 figure(3);
 plot(data2(:,1), data2(:,2));
 title('10kHZ .. 10MHz ohne Wasser');
-grid on;
+xlabel('Frequenz');
+ylabel('Amplitude');
+axis([-5 5 -6 6]);
+grid minor;
 print -dpdf 'scope_2'
 
 figure(4);
 plot(data3(:,1), data3(:,2));
 title('10kHZ .. 10MHz mit Wasser');
-grid on;
+xlabel('Frequenz');
+ylabel('Amplitude');
+axis([-5 5 -6 6]);
+grid minor;
 print -dpdf 'scope_3'
