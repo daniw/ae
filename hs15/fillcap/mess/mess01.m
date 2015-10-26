3bc4ca413c421324fb3b06a46da0cf6126d3e691
daniw <dani.winz@bluewin.ch>
2015-10-26 22:26:22 +0100

    Darstellung Amplitudenspektrum angepasst

--- hs15/fillcap/mess/mess01.m
+++ hs15/fillcap/mess/mess01.m
@@ -8,37 +8,37 @@ data2 = csvread('mess01/scope_2.csv');
 data3 = csvread('mess01/scope_3.csv');
 
 figure(1);
-plot(data0(:,1), data0(:,2));
+semilogx(10.^((data0(3:end,1)./10.*5)+5.5), data0(3:end,2));
 title('1kHZ .. 100MHz mit Wasser');
-xlabel('Frequenz');
-ylabel('Amplitude');
-axis([-5 5 -6 6]);
+xlabel('Frequenz [Hz]');
+ylabel('Amplitude [V]');
+axis([1e3 1e8 -6 6]);
 grid minor;
 print -dpdf 'scope_0'
 
 figure(2);
-plot(data1(:,1), data1(:,2));
+semilogx(10.^((data1(3:end,1)./10.*5)+5.5), data1(3:end,2));
 title('1kHZ .. 100MHz ohne Wasser');
-xlabel('Frequenz');
-ylabel('Amplitude');
-axis([-5 5 -6 6]);
+xlabel('Frequenz [Hz]');
+ylabel('Amplitude [V]');
+axis([1e3 1e8 -6 6]);
 grid minor;
 print -dpdf 'scope_1'
 
 figure(3);
-plot(data2(:,1), data2(:,2));
+semilogx(10.^((data2(3:end,1)./10.*3)+5.5), data2(3:end,2));
 title('10kHZ .. 10MHz ohne Wasser');
-xlabel('Frequenz');
-ylabel('Amplitude');
-axis([-5 5 -6 6]);
+xlabel('Frequenz [Hz]');
+ylabel('Amplitude [V]');
+axis([1e4 1e7 -6 6]);
 grid minor;
 print -dpdf 'scope_2'
 
 figure(4);
-plot(data3(:,1), data3(:,2));
+semilogx(10.^((data3(3:end,1)./10.*3)+5.5), data3(3:end,2));
 title('10kHZ .. 10MHz mit Wasser');
-xlabel('Frequenz');
-ylabel('Amplitude');
-axis([-5 5 -6 6]);
+xlabel('Frequenz [Hz]');
+ylabel('Amplitude [V]');
+axis([1e4 1e7 -6 6]);
 grid minor;
 print -dpdf 'scope_3'
