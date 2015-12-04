aa010f54dcba8501b70afbd0f04c70d9152e135f
daniw <dani.winz@bluewin.ch>
2015-12-04 22:29:54 +0100

    LED blink test firmware for tinyK20

---
+++ hs15/fillcap/fw/fillcap_disp/Sources/Events.h
@@ -0,0 +1,80 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : fillcap_disp
+**     Processor   : MK20DX128VFT5
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-12-04, 07:17, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "LED.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "WAIT.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MK20DX128EX5]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void);
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.11]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
