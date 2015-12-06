d233928b6e121165a86e9aff73f9e6359e7dcdc0
daniw <dani.winz@bluewin.ch>
2015-12-06 21:50:09 +0100

    bash scripts for reading status and voltage level as bargraph

---
+++ hs15/fillcap/sw/bash/read_bar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "F" > /dev/ttyACM1; sleep 0.01
+echo "i" > /dev/ttyACM1; sleep 0.01
+echo "l" > /dev/ttyACM1; sleep 0.01
+echo "l" > /dev/ttyACM1; sleep 0.01
+echo "c" > /dev/ttyACM1; sleep 0.01
+echo "a" > /dev/ttyACM1; sleep 0.01
+echo "p" > /dev/ttyACM1; sleep 0.01
+echo " " > /dev/ttyACM1; sleep 0.01
+echo "b" > /dev/ttyACM1; sleep 0.01
+echo "a" > /dev/ttyACM1; sleep 0.01
+echo "r" > /dev/ttyACM1; sleep 0.01
+echo "" > /dev/ttyACM1;
+
---
+++ hs15/fillcap/sw/bash/read_status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "F" > /dev/ttyACM1; sleep 0.01
+echo "i" > /dev/ttyACM1; sleep 0.01
+echo "l" > /dev/ttyACM1; sleep 0.01
+echo "l" > /dev/ttyACM1; sleep 0.01
+echo "c" > /dev/ttyACM1; sleep 0.01
+echo "a" > /dev/ttyACM1; sleep 0.01
+echo "p" > /dev/ttyACM1; sleep 0.01
+echo " " > /dev/ttyACM1; sleep 0.01
+echo "s" > /dev/ttyACM1; sleep 0.01
+echo "t" > /dev/ttyACM1; sleep 0.01
+echo "a" > /dev/ttyACM1; sleep 0.01
+echo "t" > /dev/ttyACM1; sleep 0.01
+echo "u" > /dev/ttyACM1; sleep 0.01
+echo "s" > /dev/ttyACM1; sleep 0.01
+echo "" > /dev/ttyACM1;
+
